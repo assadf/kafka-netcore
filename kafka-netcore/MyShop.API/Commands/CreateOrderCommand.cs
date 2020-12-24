70d744156873ad4a789405f8859bc28177726322
Assad Faizi <13318157+assadf@users.noreply.github.com>
2020-12-24 15:11:52 +0000

    Added Orders API (Producer)

---
+++ kafka-netcore/MyShop.API/Commands/CreateOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace MyShop.API.Commands
+{
+    public class CreateOrderCommand
+    {
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
---
+++ kafka-netcore/MyShop.API/Controllers/OrdersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyShop.API.Commands;
+using System.Threading.Tasks;
+
+namespace MyShop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("This is the orders controller");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync([FromBody]CreateOrderCommand command)
+        {
+            return Accepted();
+        }
+    }
+}
